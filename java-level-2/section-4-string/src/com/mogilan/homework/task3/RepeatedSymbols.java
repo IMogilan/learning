671c3dbeda39b5a8905d61946b3688554369db21
Ilya Mogilan <ilya.mogilan@gmail.com>
2022-04-13 10:20:46 +0300

    New options useing RegExpressions

--- java-level-2/section-4-string/src/com/mogilan/homework/task3/RepeatedSymbols.java
+++ java-level-2/section-4-string/src/com/mogilan/homework/task3/RepeatedSymbols.java
@@ -5,23 +5,43 @@ package com.mogilan.homework.task3;
 Также удалить пробелы.
 Результат привести к верхнему регистру.
 Например:
-“abc Cpddd Dio OsfWw” -> “ABCPDIOSFW”*/
+“abc Cpddd Dio OsfWw” -> “ABCPDIOSFW”
+
+
+Самостоятельное доп. задание: реализовать метод, которые удалял бы все пробелы и Все повторяющиеся символы (не только идущие друг за другом)*/
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RepeatedSymbols {
 
     public static void main(String[] args) {
         String line = "abc Cpddd Dio OsfWw";
         System.out.println(deleteRepeatedValuesAndSpaces(line));
+        System.out.println(deleteAllRepeatedValuesAndSpaces(line));
 
     }
 
     public static String deleteRepeatedValuesAndSpaces (String line) {
         String lineUpperCase = line.trim().toUpperCase().replace(" ", "");
-        System.out.println(lineUpperCase);
         StringBuilder finalLine = new StringBuilder().append(lineUpperCase.charAt(0));
         for (int i = 1; i < lineUpperCase.length(); i++) {
             if (lineUpperCase.charAt(i) == lineUpperCase.charAt(i - 1)) continue;
             finalLine.append(lineUpperCase.charAt(i));
+        }
+        return finalLine.toString();
+    }
+
+    public static String deleteAllRepeatedValuesAndSpaces (String line) {
+        String lineUpperCase = line.trim().toUpperCase().replaceAll("\s", "");
+        StringBuilder finalLine = new StringBuilder();
+        for (int i = 0; i < lineUpperCase.length(); i++) {
+            String regex = Character.valueOf(lineUpperCase.charAt(i)).toString();
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(lineUpperCase);
+            int matches = -1;
+            while (matcher.find()) matches++;
+            if (matches == 0) finalLine.append(lineUpperCase.charAt(i));
         }
         return finalLine.toString();
     }
--- java-level-2/section-4-string/src/com/mogilan/homework/task4/NumbersFromAString.java
+++ java-level-2/section-4-string/src/com/mogilan/homework/task4/NumbersFromAString.java
@@ -9,39 +9,51 @@ package com.mogilan.homework.task4;
 “Привет 8, как 1 2 твои дела? Может4, сделать 3 дело?” -> 18 (8+1+2+4+3)*/
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class NumbersFromAString {
     public static void main(String[] args) {
         String line = "Привет 8, как 1 2 твои дела? Может4, сделать 3 дело?";
         int [] allNumbersOfTheLine = getAllNumbersOfTheLine(line);
         System.out.println(Arrays.toString(allNumbersOfTheLine));
-        System.out.println(sumAllNumbersOfTheLine(allNumbersOfTheLine));
+        System.out.println(getSumAllNumbersOfTheLine(allNumbersOfTheLine));
 
+    }
+
+    private static Object getSumAllNumbersOfTheLine(int[] allNumbersOfTheLine) {
+        int result = 0;
+        for (int i : allNumbersOfTheLine) {
+            result += i;
+        }
+        return result;
     }
 
     private static int[] getAllNumbersOfTheLine(String line) {
-        int [] allNumbersOfTheLine = new int[line.length()];
-        char [] numbers = {'1', '2', '3', '4', '5', '6', '7', '8' , '9', '0'};
-        int counter = 0;
-        for (int i = 0; i < line.length(); i++) {
-            for (int j = 0; j < numbers.length; j++) {
-                if (line.charAt(i) == numbers[j]) allNumbersOfTheLine [counter++] = Character.getNumericValue(numbers[j]);
-            }
-        }
-        int [] resultArray = new int[counter];
-        for (int i = 0; i < resultArray.length; i++) {
-            resultArray [i] = allNumbersOfTheLine [i];
-        }
-        return resultArray;
+        Pattern pattern = Pattern.compile("\\d");
+        Matcher matcher = pattern.matcher(line);
+        StringBuilder lineOfNumbers = new StringBuilder();
+        while (matcher.find()) lineOfNumbers.append(matcher.group());
+        int [] result = new int[lineOfNumbers.length()];
+        for (int i = 0; i < result.length; i++) result [i] = Character.getNumericValue(lineOfNumbers.charAt(i));
+        return result;
     }
 
-    public static int sumAllNumbersOfTheLine (int [] array) {
-        int sum = 0;
-        for (int i = 0; i < array.length ; i++) {
-            sum += array [i];
-        }
-        return sum;
-    }
-
+//    Option 2 (without RegEx)
+//    private static int[] getAllNumbersOfTheLine(String line) {
+//        int [] allNumbersOfTheLine = new int[line.length()];
+//        char [] numbers = {'1', '2', '3', '4', '5', '6', '7', '8' , '9', '0'};
+//        int counter = 0;
+//        for (int i = 0; i < line.length(); i++) {
+//            for (int j = 0; j < numbers.length; j++) {
+//                if (line.charAt(i) == numbers[j]) allNumbersOfTheLine [counter++] = Character.getNumericValue(numbers[j]);
+//            }
+//        }
+//        int [] resultArray = new int[counter];
+//        for (int i = 0; i < resultArray.length; i++) {
+//            resultArray [i] = allNumbersOfTheLine [i];
+//        }
+//        return resultArray;
+//    }
 
 }
